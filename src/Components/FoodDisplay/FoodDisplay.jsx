a46e08022db8836ec06320b42f386e8c69a7ccf2
Your Name <you@example.com>
2024-10-05 18:06:00 +0300

    modified

--- src/Components/FoodDisplay/FoodDisplay.jsx
+++ src/Components/FoodDisplay/FoodDisplay.jsx
@@ -1,34 +1,36 @@
 import { useContext } from "react";
-import "./FoodDisplay.css"
+import "./FoodDisplay.css";
 import { StoreContext } from "../../Context/StoreContext";
 import FootItem from "../FootItem/FootItem";
 
-
-
 // eslint-disable-next-line react/prop-types
-const FoodDisplay = ( {categry} ) => {
+const FoodDisplay = ({ categry }) => {
+  const { food_list } = useContext(StoreContext);
 
-
-    const { food_list } = useContext(StoreContext);
-
-
-    return (
-        <div>
-            <div className="FoodDisplay">
-                <h2>Top Dishes Near You</h2>
-                <div className="foodDisplayList">
-                    {
-                        food_list.map( (item , index ) =>{
-                            if( categry === "all"  || categry === item.category ){
-                                return <FootItem  key={index}  id={item._id} name={item.name}  image={item.image} price={item.price} description={item.description} category={item.category}/>
-                            }
-                        })
-                    }
-                </div>
-            </div>
-        
+  return (
+    <div>
+      <div className="FoodDisplay">
+        <h2>Top Dishes Near You</h2>
+        <div className="foodDisplayList">
+          {food_list.map((item, index) => {
+            if (categry === "all" || categry === item.category) {
+              return (
+                <FootItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  image={item.image}
+                  price={item.price}
+                  description={item.description}
+                  category={item.category}
+                />
+              );
+            }
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
